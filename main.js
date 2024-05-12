85abe51683062612aa9e29e31ddcb1688366d827
Asadbek070 <165376801+Asadbek070@users.noreply.github.com>
2024-05-13 01:00:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+
+// // Begin(14)
+// let p = 10
+// let r = 20
+// let s = p * r**2
+// let l = 2 * p * r
+// console.log (s)
+// console.log (l)
+
+// // Begin(15)
+// let p = 3.14
+// let r = 30
+// let s = p*r**2
+// let l = 2*p*r
+// console.log(s)
+// console.log(l)
+
+// // Begin(16)
+// let x2 = 20
+// let x1 = 10
+// console.log(x2-x1)
+
+// Begin(17)
+// let a = 25
+// let b = 35
+// let c = 45
+// console.log(a+b)
+// console.log(b+c)
+// console.log(a+b+c)
+
+// // Begin(18)
+// let a = 20
+// let b = 5
+// let c = 10
+// console.log(a+c)
+// console.log(b+c)
+
+// // Begin(19)
+// let a = 10
+// let b = 20
+// let c = 30
+// let d = 48
+// console.log(a+b+c+d)
+// console.log(a+c)
+// console.log(b+d)
+
+// Begin(20)
+let x1 = 20
+let y1 = 30
+let x2 = 10
+let y2 = 50
+let a = x1+y1
+let b = x2+y2
+console.log(a)
+console.log(b)
+console.log(a**2+b**2)
